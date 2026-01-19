c32f1b6f1404f8da8d734ed14f974807b38aa251
sergiupopa-code <sergiu.popa@braze.com>
2026-01-19 15:17:59 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,14 @@
-self.importScripts('https://js.appboycdn.com/web-sdk/6.5/service-worker.js');
+/**
+ * ============================================================
+ * BRAZE WEB PUSH SERVICE WORKER
+ * ============================================================
+ *
+ * - Required for Web Push notifications
+ * - Must be served from the root scope (/)
+ * - Must be served with:
+ *   Content-Type: application/javascript
+ *
+ * This file delegates all push handling to Braze.
+ */
+
+self.importScripts("https://js.appboycdn.com/web-sdk/6.5/service-worker.js");
