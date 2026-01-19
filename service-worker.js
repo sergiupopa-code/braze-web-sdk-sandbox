fff3f1a84d2f6a1510cd537ded30acb88480f971
sergiupopa-code <sergiu.popa@braze.com>
2026-01-19 15:33:28 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,1 @@
-/**
- * Braze Web Push Service Worker
- * Must live at: /
- * Must be served over HTTPS
- */
-
-self.importScripts("https://js.appboycdn.com/web-sdk/6.5/service-worker.js");
+self.importScripts('https://js.appboycdn.com/web-sdk/6.5/service-worker.js');
