09ae6ec32d0d749a4aa887bad4b857396a454aed
sergiupopa-code <sergiu.popa@braze.com>
2026-01-20 18:05:00 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,1 @@
-braze.initialize("88884959-92fe-4bd7-ab6b-6660b1c9e859", {
-  baseUrl: "sondheim.braze.com",
-  enableLogging: true,
-  allowUserSuppliedJavascript: true,
-  manageServiceWorkerExternally: false 
-});
+self.importScripts('https://js.appboycdn.com/web-sdk/6.5/service-worker.js');
